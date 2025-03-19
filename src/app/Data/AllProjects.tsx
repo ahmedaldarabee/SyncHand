7d8569dfc8d5fd373ecc4b9f00c1790aa91804a2
ahmedaldarabee <seahmednail@gmail.com>
2025-03-19 09:46:57 +0300

    fixing add tasks problems

--- src/app/Data/AllProjects.tsx
+++ src/app/Data/AllProjects.tsx
@@ -20,34 +20,4 @@ export interface Project extends DataCommon {
 }
 
 export const projectData: Project[] = [
-    {
-        id: uuidv4(),
-        clerkUserId: "122",
-        title: "First Project",
-        icon: "List",
-        createdAt: "2024-08-26T10:00:00Z",
-        updatedAt: "2024-08-28T14:30:00Z",
-        tasks: [
-                {
-                    id: uuidv4(),
-                    title: "create first design",
-                    icon: "List",
-                    projectName: "project 1",
-                    status: "Completed",
-                    priority: "Low",
-                    createdAt: "2024-08-26T10:00:00Z",
-                    updatedAt: "2024-08-28T14:30:00Z",
-                },
-                {
-                    id: uuidv4(),
-                    title: "create second design",
-                    icon: "List",
-                    projectName: "project 1",
-                    status: "Completed",
-                    priority: "High",
-                    createdAt: "2024-08-26T10:00:00Z",
-                    updatedAt: "2024-08-28T14:30:00Z",
-                },
-            ],
-    },
 ];
