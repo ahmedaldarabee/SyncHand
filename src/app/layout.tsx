0cfef33e491528b30c3059c56305ba9baf220699
ahmedaldarabee <seahmednail@gmail.com>
2025-03-22 14:07:19 +0300

    fixing new wrongs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,15 @@ export const metadata: Metadata = {
 const clerkKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkKey) {
-  console.error("🚨 Missing Clerk Publishable Key. Make sure it's set in your environment variables.");
+  console.error("Missing Clerk Publishable Key. Make sure it's set in your environment variables.");
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>SyncHand</title>
+        <meta name="description" content="SyncHand is an AI-powered project designed to enhance productivity and efficiency for individuals and teams." />
       </head>
       <body className={`${poppins.variable} ${inter.className}`}>
         <ClerkProvider publishableKey={clerkKey}>
