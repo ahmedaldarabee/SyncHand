1f259db5c6d8dab909c68c35a9e9a855830b8e2a
ahmedaldarabee <seahmednail@gmail.com>
2025-03-23 23:41:45 +0300

    PWA Supporting New  Feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+});
+
+/** @type {import("next").NextConfig} */
+export default withPWA({
+    reactStrictMode: true,
+});
