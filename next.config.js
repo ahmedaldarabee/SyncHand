b5e1b8e1e7130d9f65a3ac9e2febaeec648582bc
ahmedaldarabee <seahmednail@gmail.com>
2025-03-24 10:15:05 +0300

    fixing new wrongs about running

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+  experimental: {
+    turbo: true,
+  },
+};
+
+module.exports = nextConfig;
