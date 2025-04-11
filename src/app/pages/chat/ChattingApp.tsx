fd1c48cb35a6f76cbaa278ff5c060dbc6d1342c9
ahmedaldarabee <seahmednail@gmail.com>
2025-04-11 16:23:25 +0300

    add sweetalert library

--- src/app/pages/chat/ChattingApp.tsx
+++ src/app/pages/chat/ChattingApp.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ChattingApp = () => {
     return (
         <div className='w-full min-h-screen flex justify-center items-center'>
-            Hello, welcome in chatting app!
+            Hello, welcome in chating app!
         </div>
     )
 }
