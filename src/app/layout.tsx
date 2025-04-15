8e509224fc81d9e574c7ac5686337a748be407c5
ahmedaldarabee <seahmednail@gmail.com>
2025-04-15 21:46:27 +0300

    PWA Problem Fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,56 @@
-"use client"
-
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ContextAppProvider from "./pages/contextApp";
 import 'ldrs/ring'
-import { Helix } from 'ldrs/react'
+// import { Helix } from 'ldrs/react'
 import 'ldrs/react/Helix.css'
-import { useEffect, useState } from "react";
-import { metadata } from "metadata";
+// import { useEffect, useState } from "react";
+import { Metadata, Viewport } from "next";
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+const APP_NAME = "SyncHand";
+const APP_DEFAULT_TITLE = "SyncHand";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "SyncHand is an AI-powered project designed to enhance productivity and efficiency for individuals and teams.";
+
+export const metadata: Metadata = {
+    applicationName: APP_NAME,
+    title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    manifest: "/manifest.json",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "default",
+        title: APP_DEFAULT_TITLE,
+    },
+    formatDetection: {
+        telephone: false,
+    },
+    openGraph: {
+        type: "website",
+        siteName: APP_NAME,
+        title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: "summary",
+        title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+};
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,16 +65,16 @@ if (!clerkKey) {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (performance.navigation.type === 1) {
-      setIsLoading(true);
-      setTimeout(() => setIsLoading(false), 1000);
-    } else {
-      setIsLoading(false);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (performance.navigation.type === 1) {
+  //     setIsLoading(true);
+  //     setTimeout(() => setIsLoading(false), 1000);
+  //   } else {
+  //     setIsLoading(false);
+  //   }
+  // }, []);
   
 
   return (
@@ -45,13 +87,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ClerkProvider>
           <ContextAppProvider>
             <body className={poppins.variable}>
-            {isLoading ? (
+            {/* {isLoading ? (
               <div className="flex justify-center items-center h-screen">
                 <Helix size={100} speed={3} color="black" />
               </div>
             ) : (
               children
-            )}
+            )} */}
+            {children}
             </body>
           </ContextAppProvider>
         </ClerkProvider>
