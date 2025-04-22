d6c918e746003c12df04bcbd1551b7ac16b18ad6
ahmedaldarabee <seahmednail@gmail.com>
2025-04-22 20:24:03 +0300

    edit chat message

--- src/components/component/ChatMessage.tsx
+++ src/components/component/ChatMessage.tsx
@@ -23,19 +23,11 @@ const ChatMessage = ({sender,message,isOwnMessage}:ChatMessageProps) => {
         <div className={`flex ${isSystemMessage ? "justify-center" : isOwnMessage ? "justify-end" : "justify-start"} mb-3`}>
             <div className={`max-w-xs px-4 py-2  ${isSystemMessage ? "bg-gray-800 text-white text-center text-xs rounded-full" : isOwnMessage ? "bg-sky-600 text-white rounded-l-lg":"bg-slate-200 rounded-b-lg text-black" }`}>
                 {/* this line refer if sender not ahmed (MY) - Owner don't show own name but if be another user show it */}
-                {
-                    !isSystemMessage && 
-                    <div className='flex gap-1'>
-                        <p className='text-sm font-bold'>{sender}</p>
-                        <Pencil className={`w-4 h-4 cursor-pointer text-white ${isOwnMessage ? "" :"hidden"}`}/>
-                    </div>
-                }
+                {!isSystemMessage && <p className='text-sm font-bold'>{sender}</p>}
                 <p>{message}</p>
             </div>
         </div>
     )
-
-    // Now, the main question, how can be know these think?
 }
 
 export default ChatMessage
