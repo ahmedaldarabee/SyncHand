6d23110a2ea394d97d012432a80f25ed5cf1e55b
Mohammad Hilal <hilal_mohammad2003@hotmail.com>
2025-05-04 23:20:19 +0300

    add local host

--- src/lib/socketClient.ts
+++ src/lib/socketClient.ts
@@ -4,4 +4,6 @@
 import { io, Socket } from "socket.io-client";
 
 // about local host, but once you deploy you app, use it link!
-export const socket: Socket = io("https://synchand.onrender.com/");
+// export const socket: Socket = io("https://synchand.onrender.com/");
+export const socket: Socket = io("http://localhost:3000/"); // local host link
+//change by mohammad hilal - as a example
