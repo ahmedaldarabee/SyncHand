e1090b7d5a432a64c097b39526b9244e0a5dff2f
ahmedaldarabee <seahmednail@gmail.com>
2026-07-04 17:44:35 +0300

    handle deployment

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { useContextApp } from '../pages/contextApp';
-import img from '../favicon.ico';
+ 
 import Image from 'next/image';
 
 const Logo = () => {
@@ -19,7 +19,7 @@ const Logo = () => {
     return (
         <div className='rounded-md h-10 w-10 flex items-center justify-center cursor-pointer'>
             
-            <Image loading="lazy" className='m-1' src={img} alt="logo of the project" width={32} height={32} />
+            <Image loading="lazy" className='m-1' src={"../favicon.ico"} alt="logo of the project" width={32} height={32} />
 
             {openSideBar && (
                 <div className='text-xl flex items-center gap-1'>
